8e32b9ca4b6ffe5fc41b6317ade0e6a484f69c5f
jprex59 <jean59paris@orange.fr>
2021-09-27 14:54:49 +0200

    btn-toggle

--- js/DarkLightMode.js
+++ js/DarkLightMode.js
@@ -1,25 +1,42 @@
 var imgPNG = document.querySelectorAll(".imgPNG");
-function themeNuitJour() {
-	const date = new Date();
-	const hour = date.getHours();
+const btnJour = document.querySelector(".btn-Jour");
+const btnNuit = document.querySelector(".btn-Nuit");
 
+const date = new Date();
+const hour = date.getHours();
+
+function Jour() {
+	console.log("light");
+	document.documentElement.style.setProperty("--ecriture", "#1e1e1e");
+	document.documentElement.style.setProperty("--box", "#ffff");
+	document.documentElement.style.setProperty("--fond", "#fffff");
+	for (let i = 0; i < imgPNG.length; i++) {
+		imgPNG[i].classList.remove("imgThemeMode");
+	}
+	btnNuit.style.zIndex = "15";
+	btnJour.style.zIndex = "10";
+}
+function Nuit() {
+	/*Dark*/
+	console.log("Dark");
+	document.documentElement.style.setProperty("--ecriture", "#ffffff");
+	document.documentElement.style.setProperty("--box", "#333333");
+	document.documentElement.style.setProperty("--fond", "#1e1e1e");
+	for (let i = 0; i < imgPNG.length; i++) {
+		imgPNG[i].classList.add("imgThemeMode");
+	}
+	btnNuit.style.zIndex = "10";
+	btnJour.style.zIndex = "15";
+}
+
+function themeNuitJour() {
 	console.log("hello");
 
-	if (hour > 5 || hour < 14) {
-		console.log("light");
-		document.documentElement.style.setProperty("--ecriture", "#1e1e1e");
-		document.documentElement.style.setProperty("--box", "#ffff");
-		document.documentElement.style.setProperty("--fond", "#fffff");
+	/*Condition*/
+	if (hour < 5 || hour > 14) {
+		Jour();
 	} else {
-		/*Dark*/
-		console.log("Dark");
-		document.documentElement.style.setProperty("--ecriture", "#ffffff");
-		document.documentElement.style.setProperty("--box", "#333333");
-		document.documentElement.style.setProperty("--fond", "#1e1e1e");
-
-		for (let i = 0; i < imgPNG.length; i++) {
-			imgPNG[i].classList.add("imgThemeMode");
-		}
+		Nuit();
 	}
 }
 themeNuitJour();
